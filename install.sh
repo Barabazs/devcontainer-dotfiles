da1880e4bed9fe0f70012830b4ab5643fded593b
Barabazs <31799121+Barabazs@users.noreply.github.com>
2024-11-05 09:17:45 +0100

    feat: add checkpoint script and update install script to include local scripts

--- install.sh
+++ install.sh
@@ -6,3 +6,16 @@ cp gitignore ~/.gitignore
 # Copy the bash_aliases
 cp bash_aliases ~/.bash_aliases
 
+# Copy the scripts
+mkdir ~/.local
+cp -r scripts ~/.local/scripts
+
+# Append to .profile
+cat <<'EOF' >>~/.profile
+if [ -d "${HOME}/.local/scripts" ] ; then
+    PATH="${HOME}/.local/scripts:$PATH"
+fi
+EOF
+
+# chmod the scripts
+chmod +x ~/.local/scripts/*
